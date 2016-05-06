732fe206cc4f1d39c850f73627331ef431ee82d9
ruanhx <48100627@qq.com>
2016-05-06 18:46:10 +0800

    选择界面 详细信息界面

--- assets/scripts/choose.js
+++ assets/scripts/choose.js
@@ -15,19 +15,14 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        var fadeOut = cc.fadeOut(5);
-        // var delay = cc.delayTime(3);
-        var finish = cc.callFunc(this.change, this);
-        this.node.runAction(cc.sequence(fadeOut,finish));
-        // this.node.runAction(cc.fadeOut(5));
-        
+        this.node.runAction(cc.fadeIn(1));
     },
-    change: function(){
-        // this.node.on(cc.Node.EventType.TOUCH_END,function(event){
-        //     cc.director.loadScene('choose');
-        // },this);
-         cc.director.loadScene('choose');
-    }
+    details:function() {
+       cc.director.loadScene('details');
+    },
+    practice:function() {
+        cc.director.loadScene('battle');
+    },
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
