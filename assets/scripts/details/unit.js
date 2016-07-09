90e66e450a00ce246128dde79015c31755822aae
ruanhx <woailuo8911@163.com>
2016-07-09 18:13:26 +0800

    详情界面优化

--- assets/scripts/details/unit.js
+++ assets/scripts/details/unit.js
@@ -10,7 +10,7 @@ cc.Class({
         this.Atb = data.atb;
         this.Atk = data.atk;
         this.name = data.name;
-        var url = cc.url.raw('resources/graphics/creature/' + data.icon + '.png');
+        var url = cc.url.raw('resources/graphics/dic/' + data.icon + '.png');
         this.node.getChildByName('pic').getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(url); 
         this.node.getChildByName('name').getComponent(cc.Label).string = this.name;
         this.node.getChildByName('hp').getComponent(cc.Label).string = "血量" + this.MaxHP;
@@ -18,6 +18,7 @@ cc.Class({
         this.node.getChildByName('move').getComponent(cc.Label).string = "移动范围" + this.Mov;
         this.node.getChildByName('rng').getComponent(cc.Label).string = "攻击范围" + this.Rng;
         this.node.getChildByName('atb').getComponent(cc.Label).string = "速度" + this.Atb;
+        this.node.getChildByName('instruction').getComponent(cc.Label).string = data.instruction;
         this.node.setPosition(loc);
         for(var i = 1; i<=3 ; ++i){
             var skl = data['skill_' + i];
